b33d81cdfe2e78991424a6a48b6f835f9c23ed5b
Matteo Borgato <borgato.matteo@gmail.com>
2013-06-05 21:59:53 -0300

    Update jquery.checkall.js
    Prop has some problems with IE7

--- jquery.checkall.js
+++ jquery.checkall.js
@@ -2,9 +2,9 @@ var list = $(".checkbox-list");
 list.find(":checkbox").change(function() {
    if ( $(this).hasClass("checkall") ) {
       if ( $(this).is(':checked') ) {
-         list.find('input:checkbox:not(:disabled)').prop('checked', 'checked');
+         list.find('input:checkbox:not(:disabled)').attr('checked', 'checked');
       } else {
-         list.find('input:checkbox:not(:disabled)').removeProp('checked');
+         list.find('input:checkbox:not(:disabled)').removeAttr('checked');
       }
    }
 });
